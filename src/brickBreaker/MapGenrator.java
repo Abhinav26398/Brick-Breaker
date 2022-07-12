5ca022fa4848813ebc5bd8b53dfab14707785bde
Abhinav Kumar Jha <abhinavjha26398@gmail.com>
2022-07-12 23:44:42 +0530

    Initial commit

---
+++ src/brickBreaker/MapGenrator.java
@@ -0,0 +1,39 @@
+package brickBreaker;
+
+import java.awt.*;
+
+public class MapGenrator {
+    public int [][] map;
+    public int brickWeidth;
+    public int brickHeight;
+    public MapGenrator(int row, int col){
+        map=new int[row][col];
+        for(int i=0; i< map.length; i++){
+            for(int j=0; j< map[0].length; j++){
+                map[i][j]=1;
+            }
+        }
+
+        brickWeidth=540/col;
+        brickHeight=150/row;
+
+    }
+
+    public void draw(Graphics2D g) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] > 0) {
+                    g.setColor(Color.white);
+                    g.fillRect(j * brickWeidth + 80, i * brickHeight + 50, brickWeidth, brickHeight);
+
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.black);
+                    g.drawRect(j * brickWeidth + 80, i * brickHeight + 50, brickWeidth, brickHeight);
+                }
+            }
+        }
+    }
+    public void setBrickValue(int value, int row, int col){
+        map [row][col]=value;
+    }
+}
